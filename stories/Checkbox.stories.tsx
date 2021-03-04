678d3d0b32c894b3f40c08faa0ba2300056618eb
netosha <antoha-surgut@yandex.ru>
2021-03-04 23:19:46 +0300

    Upd

--- stories/Checkbox.stories.tsx
+++ stories/Checkbox.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { State, Store } from '@sambego/storybook-state';
-import { addDecorator, addParameters } from '@storybook/react';
 
 import { Checkbox } from '../components';
 import { Props } from '../components/Checkbox/types';
