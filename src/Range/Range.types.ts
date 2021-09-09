d1a7d525f99b8c35d08b36afc437279445773a27
netosha <antoha-surgut@yandex.ru>
2021-09-09 16:48:49 +0300

    Added Range component
    Fixed Numeric appearance in firefox.

---
+++ src/Range/Range.types.ts
@@ -0,0 +1,12 @@
+import { InputHTMLAttributes } from 'react';
+import { MotionProps } from 'framer-motion';
+
+export interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  rounded?: boolean;
+  disabled?: boolean;
+  success?: boolean;
+  danger?: boolean;
+  warning?: boolean;
+}
+
+export type RangeProps = Props & MotionProps;
